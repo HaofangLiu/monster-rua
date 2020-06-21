62159390671f879052e2d00da61016f4abd0a0cb
HaofangLiu <494256972@qq.com>
2020-06-21 16:16:27 +0800

    Updates

---
+++ precache-manifest.47e1bd9ea108dbde0d1eb5e8cac21a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2914c713b993d06a5ad94378089e959",
+    "url": "/monster-rua.git/index.html"
+  },
+  {
+    "revision": "98c011857a08302b11ac",
+    "url": "/monster-rua.git/static/css/main.70e025e9.chunk.css"
+  },
+  {
+    "revision": "c0b6be0bc29991e6de7e",
+    "url": "/monster-rua.git/static/js/2.5a6b9a53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rua.git/static/js/2.5a6b9a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c011857a08302b11ac",
+    "url": "/monster-rua.git/static/js/main.a013202e.chunk.js"
+  },
+  {
+    "revision": "87f28ac4c3f126e10569",
+    "url": "/monster-rua.git/static/js/runtime-main.0bc44579.js"
+  }
+]);
